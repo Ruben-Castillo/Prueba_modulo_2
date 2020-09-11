d194f63a8b5b2e290c74e905da27fb60c50f789d
Rubén Ignacio Castillo Díaz <ruben.cas@outlook.com>
2020-09-11 12:52:07 -0300

    photos_count

--- prueba.rb
+++ prueba.rb
@@ -13,8 +13,10 @@ def request(adress,api_key)
 end
 def build_web_page(json_response)
   list_items=""
-  json_response['photos'].each do |hash|
-    list_items+= "\t\t\t<li><img src='#{hash['img_src']}'></li>\n"
+  json_response.each do |key,value|
+    value.each do |hash|
+      list_items+= "\t\t\t<li><img src='#{hash['img_src']}'></li>\n"
+    end
   end
   pre_list="<html>\n\t<head>\n\t</head>\n\t<body>\n\t\t<ul>"
   post_list="\t\t</ul>\n\t</body>\n</html>"
@@ -22,4 +24,18 @@ def build_web_page(json_response)
   File.write("./index.html", html)
 end
 
-puts build_web_page(request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=","QKP56G9b4L9oD7IjBxTJEa2zh1nhziTe4zxqGt5d"))
+def photos_count(json_response)
+  photos_count={"FHAZ"=>0,"RHAZ"=>0,"MAST"=>0,"CHEMCAM"=>0,"MAHLI"=>0,"MARDI"=>0,"NAVCAM"=>0,"PANCAM"=>0,"MINITES"=>0}
+  json_response.each do |key,value|
+    value.each do |hash|
+      photos_count.each do |key,value|
+        photos_count[key]+=1 if key==hash['camera']['name']
+      end
+    end
+  end
+  photos_count
+end
+
+
+
+puts photos_count(request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=","QKP56G9b4L9oD7IjBxTJEa2zh1nhziTe4zxqGt5d"))
